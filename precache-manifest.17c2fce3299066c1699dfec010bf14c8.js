8467c465cc07cfe82d2d3a5064dc4b172574e02e
Shrey Mehta <shrey.mehta@infostretch.com>
2020-01-16 15:50:34 +0530

    Updates

---
+++ precache-manifest.17c2fce3299066c1699dfec010bf14c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ccfe6f0215114663af22eda0daee114",
+    "url": "/shrey-mehta.github.io/stock-fetch/index.html"
+  },
+  {
+    "revision": "cd8d6d0248e923df0dfd",
+    "url": "/shrey-mehta.github.io/stock-fetch/static/css/main.17fc7556.chunk.css"
+  },
+  {
+    "revision": "aed4b083ae20dae41519",
+    "url": "/shrey-mehta.github.io/stock-fetch/static/js/2.e4264930.chunk.js"
+  },
+  {
+    "revision": "5a15831669afef21aa79fa2d63f7d700",
+    "url": "/shrey-mehta.github.io/stock-fetch/static/js/2.e4264930.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd8d6d0248e923df0dfd",
+    "url": "/shrey-mehta.github.io/stock-fetch/static/js/main.6e032bdd.chunk.js"
+  },
+  {
+    "revision": "de53c9bda8c5f7aa60d1",
+    "url": "/shrey-mehta.github.io/stock-fetch/static/js/runtime-main.43f5f44f.js"
+  },
+  {
+    "revision": "cb07601b2d5a4335c20880ea71b82edd",
+    "url": "/shrey-mehta.github.io/stock-fetch/static/media/giphy.cb07601b.gif"
+  },
+  {
+    "revision": "34cf53375f840ece721fc985de40d881",
+    "url": "/shrey-mehta.github.io/stock-fetch/static/media/loader.34cf5337.gif"
+  }
+]);
